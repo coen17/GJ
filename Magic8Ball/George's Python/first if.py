819b3cd5790a7bf3991b753cf08223fee2fe9d27
coen17 <geobob@hotmail.co.uk>
2016-09-20 23:20:30 +0100

    Add files via upload

---
+++ Magic8Ball/George's Python/first if.py
@@ -0,0 +1,6 @@
+age = int(input('How old are you? '))
+if age > 80:
+    print('Young old ass.')
+else:
+    print('Young looking ass.')
+    print('get off this website fuckboy.')
---
+++ Magic8Ball/George's Python/nameandlocation.py
@@ -0,0 +1,4 @@
+name = input("Hello what is your name ")
+Location  = input("Where are you from ?  ")
+print(name, "is from" , Location, )
+
