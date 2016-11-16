6be6d19bb763e6b8f23efe43c0d122f768126174
coen17 <geobob@hotmail.co.uk>
2016-11-16 11:46:59 +0000

    Add files via upload

---
+++ George's Python/int and str if statement.py
@@ -0,0 +1,20 @@
+name = input("Hello what is your name ")
+Location  = input("Where are you from ?  ")
+print(name, "is from" , Location, )
+age = int(input('How old are you? '))
+if age > 80:
+    print('Young old ass.')
+else:
+    print('Young looking ass.')
+    print('get off this website fuckboy.')
+  
+
+
+
+text = str(input("Print all information ? "))
+
+if text in ['Yes', 'yes', 'Y', 'y']: 
+    print('Your name is' , name, 'You live in' , Location, 'and you are' , age, 'Years old')
+else: 
+    print('Well Close this application')
+
