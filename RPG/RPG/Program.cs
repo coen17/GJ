c02dad248b09bca1621377d4baaeebe88caafeb3
Jay King <jayking_3@hotmail.com>
2017-05-17 01:30:09 +0100

    Update Program.cs

--- RPG/RPG/Program.cs
+++ RPG/RPG/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RPG
+namespace RPG2
 {
     class MainProgram
     {
@@ -23,7 +23,7 @@ namespace RPG
             Console.WriteLine("(1): {0} || Health: {1} Damage: {2} Defence: {3} ''{4}''", warrior.heroClassName, warrior.heroClassHealth, warrior.heroClassAttackDamage, warrior.heroClassDefense, warrior.heroClassDescription);
             Console.WriteLine("(2): {0} || Health: {1} Damage: {2} Defence: {3} ''{4}''", mage.heroClassName, mage.heroClassHealth, mage.heroClassAttackDamage, mage.heroClassDefense, mage.heroClassDescription);
             Console.WriteLine("(3): {0} || Health: {1} Damage: {2} Defence: {3} ''{4}''", rogue.heroClassName, rogue.heroClassHealth, rogue.heroClassAttackDamage, rogue.heroClassDefense, rogue.heroClassDescription);
-                              
+
             string playerClassChoice = Console.ReadLine();
 
             bool characterClassChoice = false;
@@ -32,6 +32,7 @@ namespace RPG
                 if (playerClassChoice == "Warrior")
                 {
                     Console.WriteLine("You have chosen the Warrior class.");
+                    playerStat.playerClassName = "Warrior"; 
                     playerStat.playerHealth = warrior.heroClassHealth;
                     playerStat.playerAttackDamage = warrior.heroClassAttackDamage;
                     playerStat.playerDefense = warrior.heroClassDefense;
@@ -40,7 +41,7 @@ namespace RPG
                 else if (playerClassChoice == "Mage")
                 {
                     Console.WriteLine("You have chosen the Mage class.");
-                    playerStat.playerClass = mage;
+                    playerStat.playerClassName = "Mage";
                     playerStat.playerHealth = mage.heroClassHealth;
                     playerStat.playerAttackDamage = mage.heroClassAttackDamage;
                     playerStat.playerDefense = mage.heroClassDefense;
@@ -49,7 +50,7 @@ namespace RPG
                 else if (playerClassChoice == "Rogue")
                 {
                     Console.WriteLine("You have chosen the Rogue class.");
-                    playerStat.playerClass = rogue;
+                    playerStat.playerClassName = "Rogue";
                     playerStat.playerHealth = rogue.heroClassHealth;
                     playerStat.playerAttackDamage = rogue.heroClassAttackDamage;
                     playerStat.playerDefense = rogue.heroClassDefense;
@@ -60,83 +61,85 @@ namespace RPG
                     Console.WriteLine("You must choose a class...");
                     continue;
                 }
-
-                Console.WriteLine("So...\nYou have chosen the {} class.", playerStat.playerClass);
+                Console.WriteLine(playerStat.playerClassName);
+                Console.WriteLine("So... You wish to be a {0}?", playerStat.playerClassName);
                 Console.ReadLine();
             }
         }
-    }
 
-    public class playerStatsCurrent
-    {
-        public HeroClassAttributes playerClass;
-        public string playerName;
-        public int playerLevel;
-        public int playerExperience;
-        public int playerAttackDamage;
-        public int playerHealth;
-        public int playerDefense;
-    }
-
-    public class BasePlayer
-    {
-        public HeroClassAttributes playerClass { get; set; }
-        public string playerName { get; set; }
-        public string playerClassName { get; set; }
-        public int playerLevel { get; set; }
-        public int heroClassHealth { get; set; }
-        public int heroClassAttackDamage { get; set; }
-        public int heroClassDefense { get; set; }
-
-    }
-
-    // ------------------------------------------------
-    // ----------------- Hero Classes -----------------
-    // ------------------------------------------------
-                              
-    public class HeroClassAttributes
-    {
-        public string heroClassName { get; set; }
-        public string heroClassDescription { get; set; }
-        public int heroClassAttackDamage { get; set; }
-        public int heroClassHealth { get; set; }
-        public int heroClassDefense { get; set; }
-
-    }
-
-    public class BaseWarriorClass : HeroClassAttributes
-    {
-        public BaseWarriorClass()
+        public class BasePlayer
         {
-            heroClassName = "Warrior";
-            heroClassDescription = "A strong hero.";
-            heroClassAttackDamage = 2;
-            heroClassHealth = 11;
-            heroClassDefense = 3;
-        }
-    }
+            public HeroClassAttributes playerClass { get; set; }
+            public string playerName;
+            public string playerClassName { get; set; }
+            public int playerLevel { get; set; }
+            public int heroClassHealth { get; set; }
+            public int heroClassAttackDamage { get; set; }
+            public int heroClassDefense { get; set; }
 
-    public class BaseMageClass : HeroClassAttributes
-    {
-        public BaseMageClass()
-        {
-            heroClassName = "Mage";
-            heroClassDescription = "A mystical hero.";
-            heroClassAttackDamage = 3;
-            heroClassHealth = 10;
-            heroClassDefense = 2;
         }
-    }
-    
+
+        public class playerStatsCurrent
+        {
+            public HeroClassAttributes playerClass;
+            public string playerName;
+            public string playerClassName;
+            public int playerLevel;
+            public int playerExperience;
+            public int playerAttackDamage;
+            public int playerHealth;
+            public int playerDefense;
+        }
+
+        // ------------------------------------------------
+        // ----------------- Hero Classes -----------------
+        // ------------------------------------------------
+
+        public class HeroClassAttributes
+        {
+            public string heroClassName { get; set; }
+            public string heroClassDescription { get; set; }
+            public int heroClassAttackDamage { get; set; }
+            public int heroClassHealth { get; set; }
+            public int heroClassDefense { get; set; }
+
+        }
+
+        public class BaseWarriorClass : HeroClassAttributes
+        {
+            public BaseWarriorClass()
+            {
+                heroClassName = "Warrior";
+                heroClassDescription = "A strong hero.";
+                heroClassAttackDamage = 2;
+                heroClassHealth = 11;
+                heroClassDefense = 3;
+            }
+        }
+
+        public class BaseMageClass : HeroClassAttributes
+        {
+            public BaseMageClass()
+            {
+                heroClassName = "Mage";
+                heroClassDescription = "A mystical hero.";
+                heroClassAttackDamage = 3;
+                heroClassHealth = 10;
+                heroClassDefense = 2;
+            }
+        }
+
         public class BaseRogueClass : HeroClassAttributes
-    {
-        public BaseRogueClass()
         {
-            heroClassName = "Rogue";
-            heroClassDescription = "An agile hero.";
-            heroClassAttackDamage = 3;
-            heroClassHealth = 9;
-            heroClassDefense = 3;
+            public BaseRogueClass()
+            {
+                heroClassName = "Rogue";
+                heroClassDescription = "An agile hero.";
+                heroClassAttackDamage = 3;
+                heroClassHealth = 9;
+                heroClassDefense = 3;
+            }
         }
+
     }
 }
